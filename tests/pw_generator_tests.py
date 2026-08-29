5967253fba7da7b3de6197cc728212f09231be12
James Parrott <80779630+JamesParrott@users.noreply.github.com>
2026-08-29 12:43:32 +0100

    Reduce size of test space

--- tests/pw_generator_tests.py
+++ tests/pw_generator_tests.py
@@ -15,7 +15,7 @@ from .helpers import (
 
 @pytest.mark.hypothesis
 @pytest.mark.slow
-@given(password_alts_and_num_subs=passwords_alts_and_num_subs(max_num_subs=7))
+@given(password_alts_and_num_subs=passwords_alts_and_num_subs(max_num_subs=4))
 @settings(deadline=None, suppress_health_check=[HealthCheck.too_slow, HealthCheck.data_too_large])
 def test_alt_chars_candidates_generator(password_alts_and_num_subs: tuple[str,list[tuple[int,str]],int]):
     pwd, _alts, M = password_alts_and_num_subs
