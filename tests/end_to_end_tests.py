6caa8cf2225852a47cebd188b2cba9a6e88ee0ef
James Parrott <80779630+JamesParrott@users.noreply.github.com>
2026-08-30 16:17:13 +0100

    Don't call the Path (it's not one of those fixtures you call)

--- tests/end_to_end_tests.py
+++ tests/end_to_end_tests.py
@@ -237,7 +237,6 @@ def test_aegis_checker_from_CLI(guess_and_num_subs, avdu_test_vault):
     assert result.returncode==0, f"Using Aegis encrypted vault checker, could not find 'test' from {guess=}, {num_subs=}"
 
 def test_aegi_checker_errors_from_CLI(avdu_test_vault):
-    vault = avdu_test_vault()
     num_subs = 4
     guess = "abcd", # i.e. not "test" (but not too long either)
     result = subprocess.run(
@@ -245,7 +244,7 @@ def test_aegi_checker_errors_from_CLI(avdu_test_vault):
             num_subs,
             guess,
             "--aegis",
-            str(vault),
+            str(avdu_test_vault),
         ),
         capture_output=True,
         check=False,    
