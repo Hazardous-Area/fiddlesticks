b90b2e85e43106e3df44b114add6b323c7053219
James Parrott <80779630+JamesParrott@users.noreply.github.com>
2026-09-21 11:17:09 +0100

    Correct type hints

--- dev/sketch_indexable.py
+++ dev/sketch_indexable.py
@@ -1,17 +1,15 @@
 import queue
-from collections.abc import Callable
+from collections.abc import Callable, Iterable
 from multiprocessing import Event, Process, Queue
 from multiprocessing.synchronize import Event as EventT
-from typing import NamedTuple, Self, Iterable
-
-
 
 type GuessInfo = tuple[int, tuple[str, int]]
+
 
 def _make_worker_loop_body(
     pw_found: EventT,
     guesses: Queue[GuessInfo],
-    incorrect_guesses: Queue[GuessInfo],
+    incorrect_guess_indices: Queue[int],
     checker: Callable[[str], bool],
 ):
     def work():
@@ -19,15 +17,17 @@ def _make_worker_loop_body(
             return False
 
         try:
-            guess = guesses.get(timeout=10)
+            guess_info = guesses.get(timeout=10)
         except queue.Empty:
             return False
+
+        index, (guess, _num_subs) = guess_info
 
         if checker(guess):
             pw_found.set()
             return False
 
-        incorrect_guesses.put(guess)
+        incorrect_guess_indices.put(index)
         return True
 
     return work()
@@ -36,11 +36,13 @@ def _make_worker_loop_body(
 def make_worker(
     pw_found: EventT,
     queued_guesses: Queue[GuessInfo],
-    incorrect_guesses: Queue[GuessInfo],
+    incorrect_guess_indices: Queue[int],
     checker: Callable[[str], bool],
 ):
 
-    work = _make_worker_loop_body(pw_found, queued_guesses, incorrect_guesses, checker)
+    work = _make_worker_loop_body(
+        pw_found, queued_guesses, incorrect_guess_indices, checker
+    )
 
     def worker():
         while work():
@@ -49,7 +51,7 @@ def make_worker(
     return worker
 
 
-def report(incorrect_guesses: list[str]):
+def report(incorrect_guesses: list[int]):
     pass
 
 
@@ -62,20 +64,23 @@ def parent(
     max_queue_size=10_000,
 ):
 
+    guesses = iter(guesses)
     pw_found = Event()
     queued_guesses = Queue[GuessInfo](maxsize=max_queue_size)
-    incorrect_guesses = Queue[GuessInfo](maxsize=max_queue_size)
+    incorrect_guess_indices = Queue[int](maxsize=max_queue_size)
     N = 16
 
     workers = [
         Process(
-            target=make_worker(pw_found, queued_guesses, incorrect_guesses, checker),
+            target=make_worker(
+                pw_found, queued_guesses, incorrect_guess_indices, checker
+            ),
             args=(),
         )
         for _ in range(N)
     ]
     parent_work = _make_worker_loop_body(
-        pw_found, queued_guesses, incorrect_guesses, checker
+        pw_found, queued_guesses, incorrect_guess_indices, checker
     )
 
     unqueued_candidates = True
@@ -88,19 +93,21 @@ def parent(
         if unqueued_candidates and approx_queue_size <= min_queue_size:
             # Or while workers not timed out
             guess_info = next(guesses, None)
-            if guess is None:
+            if guess_info is None:
                 unqueued_candidates = False
             else:
                 queued_guesses.put(guess_info)
                 continue
 
-        newly_incorrect_guesses = []
+        indices: list[int] = []
         while True:
             try:
-                newly_incorrect_guesses.append(incorrect_guesses.get_nowait())
+                index = incorrect_guess_indices.get_nowait()
             except queue.Empty:
                 break
-        report(newly_incorrect_guesses)
+            indices.append(index)
+
+        report(indices)
 
         more_work = parent_work()
         if not more_work:
